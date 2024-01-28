cdc3e71c49b12f61cea703ae2ff6cb15ef55ab5e
harshitha <pottiharshitha24@gmail.com>
2024-01-28 18:40:47 +0530

    Student details are displayed

--- Student.java
+++ Student.java
@@ -1,22 +1,28 @@
-import java.util.Scanner;
 import java.time.LocalDate;
 import java.time.Period;
+import java.time.format.DateTimeFormatter;
 
-public class Student{
-    public static void main(String []args){
-    String name;
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter the student name:");
-    name = sc.nextLine();
+public class Student {
+    public String name;
+    public String dob;
 
-    LocalDate dateOfBirth = LocalDate.of(2004, 7, 24);
+    public void displayName() {
+        System.out.println("Student Name: " + name);
+    }
 
-    // Get the current date
-    LocalDate currentDate = LocalDate.now();
-
-    // Calculate the age of the student
-    Period age = Period.between(dateOfBirth, currentDate);
-
-    System.out.println("Age: " + age.getYears() + " years"); 
+    public void displayAge(String dob) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        LocalDate birthDate = LocalDate.parse(dob, formatter);
+        LocalDate currentDate = LocalDate.now();
+        Period period = Period.between(birthDate, currentDate);
+        int age = period.getYears();
+        System.out.println("Age of student: " + age +"years");
+    }
+    public static void main(String[] args) {
+        Courses s = new Courses();
+        s.name = "Radha Harshitha";
+        s.displayName();
+        s.displayAge("24-07-2004");
+        s.displayStudentMarks();
 }
 }
